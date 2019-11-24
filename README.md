f6988ba62f1c5c07521f6accb4612933659168a5
matheus-alvess <matheus.maatt@gmail.com>
2019-11-23 21:03:59 -0300

    add server files local with express

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-DOCKER RUN APP COMMAND: 
+Sequelize commands:
+
+Create Migration run:
+`yarn sequelize migration:create --name create-files`
+
+DOCKER RUN APP COMMAND:
 
 docker build -t sucrase-app .
 
 docker run \
-		--net='host' \
-		--name=sucrase-app \
-    --log-opt max-size=10m \
-    --log-opt max-file=5 \
-		-p 3333:3333 \
-		-d \
-		--restart=unless-stopped sucrase-app
+ --net='host' \
+ --name=sucrase-app \
+ --log-opt max-size=10m \
+ --log-opt max-file=5 \
+ -p 3333:3333 \
+ -d \
+ --restart=unless-stopped sucrase-app
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderControllers';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/ping', (req, res) => {
   res.json({ message: 'ping' });
@@ -14,6 +19,10 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
