e1df1140060c8141009c4d4e466d23ab9b976e66
matheus-alvess <matheus.maatt@gmail.com>
2020-01-04 00:46:56 -0300

    finalizando rota de listagem de horarios disponiveis fornecedor

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -26,6 +27,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/appointments', AppointmentController.store);
 
